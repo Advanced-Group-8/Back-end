10e4100477735f8fc192a6766b487822b3c06fd7
Eric Grahn <eric.grahn@chasacademy.se>
2025-09-18 18:16:59 +0200

    Test of startServer function

--- api/src/utils/validation.ts
+++
@@ -1,7 +0,0 @@
-import { ZodError } from "zod";
-
-export const buildZodValidationErrors = (err: unknown) => {
-  if (err instanceof ZodError) {
-    return err.errors.map(({ path, message }) => ({ path, message }));
-  }
-};
