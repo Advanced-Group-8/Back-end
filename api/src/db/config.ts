3607af6e80d6bba2614e8dae0826f0c195fb830d
oscar <oscar.burman005@gmail.com>
2025-09-18 10:16:17 +0200

    Updated conenction to db

--- api/src/db/config.ts
+++ api/src/db/config.ts
@@ -7,8 +7,7 @@ dotenv.config();
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-console.log(DATABASE_URL);
-
 export const pool = new Pool({
   connectionString: DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
