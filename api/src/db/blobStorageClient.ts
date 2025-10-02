c21a32f01cc5742fce92318fca397f6867e6a6c0
oscar <oscar.burman005@gmail.com>
2025-10-02 16:53:28 +0200

    Created storage client

---
+++ api/src/db/blobStorageClient.ts
@@ -0,0 +1,10 @@
+import { BlobServiceClient } from "@azure/storage-blob";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING!);
+const containerName = "images";
+const containerClient = blobServiceClient.getContainerClient(containerName);
