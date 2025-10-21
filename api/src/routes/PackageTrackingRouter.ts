e5efb9daf4b768289290276504408cd8df9dad8c
oscar <oscar.burman005@gmail.com>
2025-10-21 15:48:45 +0200

    Opened endpoints for adding data

--- api/src/routes/PackageTrackingRouter.ts
+++ api/src/routes/PackageTrackingRouter.ts
@@ -1,14 +1,13 @@
 import express from "express";
 import PackageTrackingController from "@/src/controllers/PackageTrackingController.js";
 import PackageTrackingValidator from "@/src/validators/PackageTrackingValidator.js";
-import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", AuthMiddleware.authenticate, PackageTrackingValidator.create, PackageTrackingController.create);
+router.post("/", PackageTrackingValidator.create, PackageTrackingController.create);
 
-router.get("/:deviceId", AuthMiddleware.authenticate, PackageTrackingController.getByDeviceId);
+router.get("/:deviceId", PackageTrackingController.getByDeviceId);
 
-router.get("/", AuthMiddleware.authenticate, PackageTrackingController.getAllGroupedByDeviceId);
+router.get("/", PackageTrackingController.getAllGroupedByDeviceId);
 
 export default router;
