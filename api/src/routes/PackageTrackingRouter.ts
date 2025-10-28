6645f8731b456064f2d8be49336d2d57497bb4e5
rebeccalindman <rebeccalindman@icloud.com>
2025-10-28 17:41:51 +0100

    Update PackageTrackingRouter.ts

--- api/src/routes/PackageTrackingRouter.ts
+++ api/src/routes/PackageTrackingRouter.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import PackageTrackingController from "@/src/controllers/PackageTrackingController.js";
 import PackageTrackingValidator from "@/src/validators/PackageTrackingValidator.js";
+import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", PackageTrackingValidator.create, PackageTrackingController.create);
+router.post("/", AuthMiddleware.authenticate, PackageTrackingValidator.create, PackageTrackingController.create);
 
-router.get("/:deviceId", PackageTrackingController.getByDeviceId);
+router.get("/:deviceId", AuthMiddleware.authenticate, PackageTrackingController.getByDeviceId);
 
-router.get("/", PackageTrackingController.getAllGroupedByDeviceId);
+router.get("/", AuthMiddleware.authenticate, PackageTrackingController.getAllGroupedByDeviceId);
 
 export default router;
